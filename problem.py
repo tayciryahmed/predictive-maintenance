085e0449baec3f6c7dc994b5e1d574d3d7c256cc
tayciryahmed <taycir.yahmed@gmail.com>
2017-12-22 13:36:25 +0100

    add problem.py & sample submission

---
+++ problem.py
@@ -0,0 +1,52 @@
+import os
+import pandas as pd
+import rampwf as rw
+from sklearn.model_selection import ShuffleSplit
+
+import numpy as np
+from sklearn.metrics import mean_absolute_error
+
+class mean_error(rw.score_types.BaseScoreType):
+    is_lower_the_better = True
+    minimum = 0.0
+    maximum = float('inf')
+
+    def __init__(self, name='rmse', precision=2):
+        self.name = name
+        self.precision = precision
+
+    def __call__(self, y_true, y_pred):
+	return mean_absolute_error(y_true, y_pred) / np.mean(y_true)
+
+problem_title = 'Predictive maintenance'
+_target_column_name = 'RUL'
+# A type (class) which will be used to create wrapper objects for y_pred
+Predictions = rw.prediction_types.make_regression()
+# An object implementing the workflow
+workflow = rw.workflows.FeatureExtractorRegressor()
+
+score_types = [
+    mean_error(name='mean error', precision=3),
+]
+
+
+def get_cv(X, y):
+    cv = ShuffleSplit(n_splits=3, test_size=0.5, random_state=57)
+    return cv.split(X)
+
+
+def _read_data(path, f_name):
+    data = pd.read_csv(os.path.join(path, 'data', f_name))
+    y_array = data[_target_column_name].values
+    X_df = data.drop(_target_column_name, axis=1)
+    return X_df, y_array
+
+
+def get_train_data(path='.'):
+    f_name = 'train'
+    return _read_data(path, f_name)
+
+
+def get_test_data(path='.'):
+    f_name = 'test'
+    return _read_data(path, f_name)
---
+++ submissions/starting_kit/feature_extractor.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os
+
+
+class FeatureExtractor(object):
+    def __init__(self):
+        pass
+
+    def fit(self, X_df, y_array):
+        pass
+
+    def transform(self, X_df):
+	#X = X_df.drop(columns=['os3'], axis=1)
+	return X
