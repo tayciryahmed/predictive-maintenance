1c0081028fae1c7ed08bb3666f8bfae799221bb1
Taycir Yahmed <taycir.yahmed@gmail.com>
2017-12-24 19:12:01 +0100

    change seperator in read csv

--- problem.py
+++ problem.py
@@ -38,7 +38,7 @@ def get_cv(X, y):
 
 
 def _read_data(path, f_name):
-    data = pd.read_csv(os.path.join(path, 'data', f_name))
+    data = pd.read_csv(os.path.join(path, 'data', f_name), sep=" ")
     y_array = data[_target_column_name].values
     X_df = data.drop(_target_column_name, axis=1)
     return X_df, y_array
